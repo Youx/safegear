56aa19c7b7de6ab2106a4198d89ab6206e16a6c7
Hugo Camboulive <hugo@cambou.live>
2024-07-13 22:31:15 +0200

    backend: embed migrations

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
 use axum::{routing::get, Router};
 use diesel_async::pooled_connection::deadpool;
+use diesel_migrations::{embed_migrations, EmbeddedMigrations};
 
 async fn hello_world() -> &'static str {
     "Hello, world!"
 }
+
+pub const MIGRATIONS: EmbeddedMigrations = embed_migrations!();
 
 #[shuttle_runtime::main]
 async fn main(
