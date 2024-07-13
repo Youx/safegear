5b57ff6b707895393d9eceeee7b005fe02902242
Hugo Camboulive <hugo@cambou.live>
2024-07-13 23:03:54 +0200

    backend: add database schema

---
+++ src/schema.rs
@@ -0,0 +1,46 @@
+// @generated automatically by Diesel CLI.
+
+diesel::table! {
+    events (id) {
+        id -> Int8,
+        item_id -> Int8,
+        parent_id -> Nullable<Int8>,
+        ts -> Timestamp,
+        data -> Jsonb,
+    }
+}
+
+diesel::table! {
+    items (id) {
+        id -> Int8,
+        name -> Varchar,
+        inspection_period_days -> Nullable<Interval>,
+    }
+}
+
+diesel::table! {
+    items_tags (id) {
+        id -> Int8,
+        item_id -> Int8,
+        tag_id -> Int8,
+    }
+}
+
+diesel::table! {
+    tags (id) {
+        id -> Int8,
+        name -> Varchar,
+        color -> Varchar,
+    }
+}
+
+diesel::joinable!(events -> items (item_id));
+diesel::joinable!(items_tags -> items (item_id));
+diesel::joinable!(items_tags -> tags (tag_id));
+
+diesel::allow_tables_to_appear_in_same_query!(
+    events,
+    items,
+    items_tags,
+    tags,
+);
