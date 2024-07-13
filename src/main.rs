9e82a508a7d9ead952fdd8ebae6ec2b19e882745
Hugo Camboulive <hugo@cambou.live>
2024-07-13 20:10:56 +0200

    shuttle: add postgres db with diesel async

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,14 @@
 use axum::{routing::get, Router};
+use diesel_async::pooled_connection::deadpool;
 
 async fn hello_world() -> &'static str {
     "Hello, world!"
 }
 
 #[shuttle_runtime::main]
-async fn main() -> shuttle_axum::ShuttleAxum {
+async fn main(
+    #[shuttle_shared_db::Postgres] db_pool: deadpool::Pool<diesel_async::AsyncPgConnection>,
+) -> shuttle_axum::ShuttleAxum {
     let router = Router::new().route("/", get(hello_world));
 
     Ok(router.into())
