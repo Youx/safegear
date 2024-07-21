f4d2faa52d7ac4340c516794ca49ed90c5098407
Hugo Camboulive <hugo@cambou.live>
2024-07-21 10:21:25 +0200

    frontend: add primevue + primeicons + primeflex

--- webui/src/main.ts
+++ webui/src/main.ts
@@ -1,7 +1,11 @@
+import 'primeicons/primeicons.css'
+import 'primeflex/primeflex.css'
 import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import PrimeVue from 'primevue/config'
+import Aura from '@primevue/themes/aura'
 
 import App from './App.vue'
 import router from './router'
@@ -10,5 +14,13 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(PrimeVue, {
+    theme: {
+      preset: Aura,
+      options: {
+        darkModeSelector: 'system'
+      }
+    }
+})
 
 app.mount('#app')
