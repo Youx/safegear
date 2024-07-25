62d080ef305bcc91ff28e390c702739bf4e07818
Hugo Camboulive <hugo@cambou.live>
2024-07-25 09:30:35 +0200

    frontend: use web hash history to avoid 404

--- webui/src/router/index.ts
+++ webui/src/router/index.ts
@@ -1,11 +1,11 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import ItemList from '../views/ItemList.vue'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import ItemCreate from '../views/ItemCreate.vue'
 import ItemDetails from '../views/ItemDetails.vue'
+import ItemList from '../views/ItemList.vue'
 import TagList from '../views/TagList.vue'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/items',
